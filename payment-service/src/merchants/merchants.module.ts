9a4a294356b1c8772fdae16f848a8909b8936707
@Bluruwu <juandiegotres@hotmail.com>
2026-04-26 17:35:54 -0500

    refactor: Implement native pg adapter and isolate modules

--- payment-service/src/merchants/merchants.module.ts
+++ payment-service/src/merchants/merchants.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MerchantsService } from './merchants.service';
 import { MerchantsController } from './merchants.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MerchantsController],
   providers: [MerchantsService],
 })
