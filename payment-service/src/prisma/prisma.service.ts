0e7ebbdc1074fa73f2189a920ae5bb39ac43f51a
@Bluruwu <juandiegotres@hotmail.com>
2026-04-26 18:04:37 -0500

    chore: add missing dontenv for pg pool

--- payment-service/src/prisma/prisma.service.ts
+++ payment-service/src/prisma/prisma.service.ts
@@ -2,6 +2,8 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
